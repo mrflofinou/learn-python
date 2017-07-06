a2c55e3eba3d604a46f085d83c4c8c2eb866e9f6
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-06 14:54:28 +0200

    exercise for Michael

---
+++ list_comprehension.py
@@ -0,0 +1,27 @@
+##########################
+# List comprehension 101 #
+##########################
+
+numbers = [1, 4, 10, 3, 30, 2, 66, 20, 30]
+
+words = ['jacqueline',
+        'Robert',
+        'tulipes en fleurs',
+        'rue des chemins',
+        'palmiers de Dubai',
+        'Paris la nuit',
+        'bracelet',
+        'jantes',
+        'bois foncé']
+
+# multiply per 2 all numbers
+
+# convert all numbers into string
+
+# keep words that does not contain blank spaces.
+
+# create a new list with even numbers only (nombres pairs).
+# Use a conditional structure!
+
+# create a new list with numbers and words as tuples.
+# should look like: [('jacqueline', 1), ('robert', 4)]
